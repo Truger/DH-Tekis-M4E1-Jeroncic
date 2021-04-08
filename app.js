c4baae3e8d48d504706f626b281d82e1afe5314d
Truger <ezequieljeroncic@hotmail.com>
2021-04-08 01:13:44 -0300

    Segundo Commit

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 const path = require('path');
+const puerto = process.env.PORT
 console.log('probando')
 app.use(express.static('public'));
 
@@ -10,6 +11,6 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './views/home.html'));
 });
 
-app.listen(port, () => {
-    console.log('Example app listening on port '+ port);
+app.listen(puerto || 3000, function() {
+    console.log("Servidor corriendo en el puerto 3000");
 });
